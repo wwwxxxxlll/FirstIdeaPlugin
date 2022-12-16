74d896ca7d971ba2cda38c29b4d3f8662f37b316
GitHub Action <action@github.com>
2022-12-16 14:02:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/wwwxxxxlll/firstideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wwwxxxxlll/firstideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wwwxxxxlll.firstideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wwwxxxxlll.firstideaplugin.MyBundle
 
 class MyApplicationService {
 
